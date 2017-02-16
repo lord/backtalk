854f5defe9299ba68fb6d312bfa6e5707bfb23b0
Robert Lord <robert@lord.io>
2017-02-16 15:14:22 -0600

    Working websocket example

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
+extern crate ws;
+
 #[cfg(test)]
 mod tests {
+    use ws;
     #[test]
     fn it_works() {
+        ws::listen("127.0.0.1:3012", |out| {
+            move |msg: ws::Message| {
+                println!("{:?}", msg);
+                let msg_str = msg.as_text().unwrap();
+                let foo = "backtalk: ".to_string() + msg_str;
+                out.send(ws::Message::text(foo))
+            }
+        });
     }
 }
